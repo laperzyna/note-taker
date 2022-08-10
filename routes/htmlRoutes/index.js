1ddcc50102d75a8f62aed9a66614d32f41d53551
laperzyna <90733086+laperzyna@users.noreply.github.com>
2022-08-10 16:52:52 -0700

    .

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -2,14 +2,14 @@ const path = require('path');
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'))
+    res.sendFile(path.join(__dirname, '../..public/index.html'))
 });
 
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/notes.html'))
-   });
+    res.sendFile(path.join(__dirname, '../..public/notes.html'))
+});
 
-router.get ('*', (req,res) => {
+router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'))
 });
 
