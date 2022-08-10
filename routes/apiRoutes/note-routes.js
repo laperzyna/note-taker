02d5a9d952520b233de60b43e0f1df35e2033944
laperzyna <90733086+laperzyna@users.noreply.github.com>
2022-08-10 16:49:19 -0700

    added routes for the notes that include: get, post, and delete. Now just have to adjust the db file.

---
+++ routes/apiRoutes/note-routes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const store = require('../../db/store');
+
+router.get('/notes', (req, res) => {
+    store 
+      .getNotes()
+      .then(notes => {
+        res.json(notes)
+      })
+      .catch(err => {
+        res.status(500).json(err)
+      })
+    });
+
+router.post('/notes', (req, res) => {
+    store 
+      .addNotes(req.body)
+      .then(note => {
+        res.json(note)
+      })
+  });
+
+router.delete('/notes/:id', (req, res) =>{
+  store
+  .removeNotes(req.params.id)
+  .then(()=> res.json({ok: true}))
+      
+});
+
+module.exports = router;
