c54eb393b9f82997a5b3ffef8017a6bc4df0bda9
laperzyna <90733086+laperzyna@users.noreply.github.com>
2022-08-10 17:07:03 -0700

    added functionality for adding noteds and removing them

--- db/note.js
+++ db/note.js
@@ -1,7 +1,6 @@
 const util = require('util');
 const fs = require('fs');
 const { v4: uuid4 } = require("uuid");
-
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
@@ -12,14 +11,30 @@ class Store {
     write(note) {
         return writeFileAsync('db/db.json', JSON.stringify(note))
     }
+    addNotes(note) {
+        const { title, text } = note
+        if (!title || !text) {
+            throw new Error("title and text cannot be blank")
+        }
 
+        const newNotes = { title, text, id: uuid4() }
+
+        return this.getNotes()
+            .then(notes => [...notes, newNotes])
+            .then(updateNotes => this.write(updateNotes))
+            .then(() => this.addNotes)
+    }
     getNotes() {
         return this.read()
             .then(notes => {
                 return JSON.parse(notes) || [];
             })
     }
-
+    removeNotes(id) {
+        return this.getNotes()
+            .then(notes => notes.filter(note => note.id !== id))
+            .then(keptNotes => this.write(keptNotes))
+    }
 }
 
 module.exports = new Store;
